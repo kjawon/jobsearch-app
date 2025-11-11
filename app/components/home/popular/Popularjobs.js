4dfcae15b4cf38ed31c64970cc06c620602b1ff6
kjawon <wodnjsdl0365@naver.com>
2025-11-11 10:09:39 +0900

    11111009

--- app/components/home/popular/Popularjobs.js
+++ app/components/home/popular/Popularjobs.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
 import styles from './popularjobs.style';
+import { COLORS } from '../../../../constants';
+
 
 const Popularjobs = () => {
+    const router = useRouter();
+    const isLoading = false;
+    const error = false;
+    
     return (
-        <View>
-            <Text>Popularjobs</Text>
+        <View style={styles.container}>
+            <View style={styles.header}></View>
+               <Text style={styles.headerTitle}>Popular jobs</Text>
+               <TouchableOpacity>
+                  <Text style={styles.headerBtn}>Show all</Text>
+               </TouchableOpacity>
+            <View style = {styles.cardsContainer}>
+                {isLoading ? (
+                    <ActivityIndicator size='large' color={COLORS.primary}/>
+                ) : error?(
+                    <Text>Something went wrong</Text>
+                ) : (
+                    <FlatList/>
+                )}
+                
+            </View>   
         </View>
-    )
-}
+    );
+};
+
 
 export default Popularjobs;
--- app/components/index.js
+++ app/components/index.js
@@ -1,6 +1,10 @@
 import ScreenHeaderBtn from "./header/ScreenHeaderBtn";
 import Welcome from "./home/welcome/Welcome";
+import Popularjobs from "./home/popular/Popularjobs";
+import Nearbyjobs from "./home/nearby/Nearbyjobs"
 export{
     ScreenHeaderBtn,
-    Welcome
+    Welcome,
+    Popularjobs,
+    Nearbyjobs
 };
