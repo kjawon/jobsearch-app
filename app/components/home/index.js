9e4eba216092a137099ee126d2f6b32d1cd34cb9
kjawon <wodnjsdl0365@naver.com>
2025-11-11 11:11:51 +0900

    11111111

---
+++ app/components/home/index.js
@@ -0,0 +1,10 @@
+import ScreenHeaderBtn from "../header/ScreenHeaderBtn";
+import Welcome from "./welcome/Welcome";
+import Popularjobs from "./popular/Popularjobs";
+import Nearbyjobs from "./nearby/Nearbyjobs"
+export{
+    ScreenHeaderBtn,
+    Welcome,
+    Popularjobs,
+    Nearbyjobs
+};
